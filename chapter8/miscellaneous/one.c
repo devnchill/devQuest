d7905ca21062077815d363f4f2860ba62448bc33
0xDev <169875752+devnchill@users.noreply.github.com>
2025-01-24 17:54:53 +0530

    chore: structured chapter and solved a basic array question

---
+++ chapter8/miscellaneous/one.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void) {
+  printf("Enter 10 numbers :");
+  int i = 0;
+  int arr[10];
+  while (i < 10) {
+    scanf("%d", &arr[i]);
+    i++;
+  }
+  printf("In reverse Order: ");
+  for (i = 9; i >= 0; i--) {
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
