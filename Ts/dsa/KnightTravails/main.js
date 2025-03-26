70f7a752dd2b6bb4a77beeeef287a8cc217ab45c
0xDev <169875752+devnchill@users.noreply.github.com>
2025-03-26 23:24:51 +0530

    chore: cleanup unneccesary file

--- Ts/dsa/KnightTravails/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Graph_1 = require("./components/Graph");
-var graph = new Graph_1.Graph([1, 1], [3, 3]);
-console.log(graph.traverse());
