499349ce07271455fed9d906307a96b27fa094b5
0xDev <169875752+devnchill@users.noreply.github.com>
2025-03-29 00:12:49 +0530

    chore: read about structs

---
+++ Golang/Variables/Variable.md
@@ -0,0 +1,20 @@
+- `bool`: a boolean value, either true or false
+- `string`: a sequence of characters
+- `int`: a signed integer
+- `float64`: a floating-point number
+- `byte`: exactly what it sounds like: 8 bits of data
+
+### Declaring a variable
+
+1. Just declare
+
+```go
+var variableName type
+variableName = valueAssigned
+```
+
+2. Short Variable Declaration
+
+```go
+variableName:= value
+```
---
+++ Golang/Variables/main.go
@@ -0,0 +1,31 @@
+// package main
+//
+// import "fmt"
+//
+//	func main() {
+//		averageOpenRate, displayMessage := 0.23, "is the average rate of your message"
+//		fmt.Println(averageOpenRate, displayMessage)
+//	}
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	const premiumPlanName = "Premium Plan"
+// 	const basicPlanName = "Basic Plan"
+//
+// 	fmt.Println("plan:", premiumPlanName)
+// 	fmt.Println("plan:", basicPlanName)
+// }
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	const secondsInMinute = 60
+// 	const minutesInHour = 60
+// 	const secondsInHour = minutesInHour * secondsInMinute
+// 	// don't edit below this line
+// 	fmt.Println("number of seconds in an hour:", secondsInHour)
+// }
+
