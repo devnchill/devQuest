f06af7b4889267ca157b379663ca4c54d9b33b67
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-09 23:16:32 +0530

    feat: working on CCWC and learned about interface in go

--- C/Fundamentals/exercise/2.c
+++ C/Fundamentals/exercise/2.c
@@ -1,9 +1,7 @@
 // Below is a directive . They don't require semicolon at the end;
 #include <stdio.h>
 /*Rest all are statements*/
-int main(void) {
-  printf("Parkinson's Law: \nWork expands so as to ");
-  printf("fill the time\n");
-  printf("available for its completion. \n");
-  return 0;
-}
+printf("Parkinson's Law: \nWork expands so as to ");
+printf("fill the time\n");
+printf("available for its completion. \n");
+int main(void) { return 0; }
---
+++ Golang/Interface/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sendMessage(msg message) (string, int) {
+	length := len(msg.getMessage())
+	return msg.getMessage(), length * 3
+}
+
+type message interface {
+	getMessage() string
+}
+
+// don't edit below this line
+
+type birthdayMessage struct {
+	birthdayTime  time.Time
+	recipientName string
+}
+
+func (bm birthdayMessage) getMessage() string {
+	return fmt.Sprintf("Hi %s, it is your birthday on %s", bm.recipientName, bm.birthdayTime.Format(time.RFC3339))
+}
+
+type sendingReport struct {
+	reportName    string
+	numberOfSends int
+}
+
+func (sr sendingReport) getMessage() string {
+	return fmt.Sprintf(`Your "%s" report is ready. You've sent %v messages.`, sr.reportName, sr.numberOfSends)
+}
+
+// L2
+type employee interface {
+	getName() string
+	getSalary() int
+}
+
+type contractor struct {
+	name         string
+	hourlyPay    int
+	hoursPerYear int
+}
+
+func (c contractor) getName() string {
+	return c.name
+}
+
+func (c contractor) getSalary() int {
+	return c.hourlyPay * c.hoursPerYear
+}
+
+type fullTime struct {
+	name   string
+	salary int
+}
+
+func (ft fullTime) getSalary() int {
+	return ft.salary
+}
+
+func (ft fullTime) getName() string {
+	return ft.name
+}
+
+// L10
+
+func getExpenseReport(e expense) (string, float64) {
+	email, ok := e.(email)
+	if !ok {
+		sms, ok := e.(sms)
+		if !ok {
+			return "", 0.0
+		}
+		return sms.toPhoneNumber, sms.cost()
+	}
+	return email.toAddress, email.cost()
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
