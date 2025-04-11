e5d4a18d698fe4bd95e3a8d5a4a94ec084e8c730
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-12 02:33:32 +0530

    feat: finished reading basics of error in go

--- Golang/Errors/main.go
+++ Golang/Errors/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 	// fmt.Printf("%v", nil)
 	// fmt.Print(getSMSErrorString(32, "AK"))
-
 }
 
 func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
@@ -33,4 +33,45 @@ func sendSMS(message string) (int, error) {
 
 func getSMSErrorString(cost float64, recipient string) string {
 	return fmt.Sprintf("SMS that costs $%v to be sent to '%v' can not be sent", cost, recipient)
+}
+
+// type divideError struct {
+// 	dividend float64
+// }
+
+// func (e divideError) Error() string {
+// 	return fmt.Sprintf("can not divide %v by zero", e.dividend)
+// }
+
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		err := errors.New("no dividing by 0")
+		return 0, err
+	}
+	return dividend / divisor, nil
+}
+
+func maxMessages(thresh int) int {
+	cost := 0
+	var i int
+	for i = 0; cost <= thresh; i++ {
+		cost += 100 + i
+	}
+	return i - 1
+}
+
+func fizzbuzz() {
+	i := 0
+	for i = 0; i < 100; i++ {
+		if i%3 == 0 {
+			fmt.Println("fizz")
+		}
+		if i%5 == 0 {
+			fmt.Println("buzz")
+		}
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		}
+		fmt.Println(i)
+	}
 }
