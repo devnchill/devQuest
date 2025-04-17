6e05577e5422b64bce1e94ff467cba29c631b6df
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-17 18:26:10 +0530

    feat: finished learning basics of GO

---
+++ Golang/Mutexes/1.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mu     *sync.RWMutex
+}
+
+func (sc safeCounter) inc(key string) {
+	sc.mu.RLock()
+	defer sc.mu.RUnlock()
+	sc.slowIncrement(key)
+}
+
+func (sc safeCounter) val(key string) int {
+	sc.mu.RLock()
+	defer sc.mu.RUnlock()
+	return sc.slowVal(key)
+}
+
+// don't touch below this line
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+func (sc safeCounter) slowVal(key string) int {
+	time.Sleep(time.Microsecond)
+	return sc.counts[key]
+}
