e6c7e045fbffa0149af2006aac79877cd12c7796
0xDev <169875752+devnchill@users.noreply.github.com>
2025-06-03 11:53:16 +0530

    chore: removed rustlings

--- Rust/rustlings/exercises/01_variables/variables3.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // TODO: Change the line below to fix the compiler error.
-    let x: i32 = 22;
-
-    println!("Number {x}");
-}
--- Rust/rustlings/exercises/02_functions/functions1.rs
+++
@@ -1,8 +0,0 @@
-// TODO: Add some function with the name `call_me` without arguments or a return value.
-
-fn main() {
-    fn call_me() {
-        println!("Hello World");
-    }
-    call_me(); // Don't change this line
-}
